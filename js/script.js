505c6501bbba610f08e8ea0f554e534f973b71ab
GENT HETEMI <GENTHETEMI7@GMAIL.COM>
2023-12-07 00:59:45 +0100

    Disa Ndryshime Drastike

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let formBtn = document.querySelector('#login-btn');
-let loginForm = document.querySelector('.login-form-container');
+
 let formClose = document.querySelector('#form-close');
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
@@ -9,7 +9,7 @@ let videoBtn = document.querySelectorAll('.vid-btn');
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
-    loginForm.classList.remove('active');
+    
 }
 
 menu.addEventListener('click', () => {
@@ -17,13 +17,8 @@ menu.addEventListener('click', () => {
     navbar.classList.toggle('active');
 });
 
-formBtn.addEventListener('click', () => {
-    loginForm.classList.add('active');
-});
 
-formClose.addEventListener('click', () => {
-    loginForm.classList.remove('active');
-});
+
 
 videoBtn.forEach(btn =>{
     btn.addEventListener('click', () =>{
@@ -34,46 +29,3 @@ videoBtn.forEach(btn =>{
     });
 });
 
-
-
-
-
-let signinButton =document.getElementById('signinButton');
-let incorrectEmail = document.getElementById('incorrectEmail'); 
-let emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-signinButton.addEventListener('click', function(event){
-    
-    let email = document.getElementById('email').value;
-
-    if(email == ''){
-        incorrectEmail.innerText='Fill the Email field!';
-        event.preventDefault();
-    }else{
-        if(emailRegex.test(email) == true){
-
-        }else{
-            incorrectEmail.innerText='Incorrect Email!';
-            event.preventDefault();
-        }
-    }
-})
-let incorrectPassword = document.getElementById('incorrectPassword');
-let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
-
-signinButton.addEventListener('click', function(event){
-
-    let password = document.getElementById('password').value;
-
-    if(password == ''){
-        incorrectPassword.innerText = 'Fill the Password field!';
-        event.preventDefault();
-    }else{
-        if(passwordRegex.test(password) == true){
-            return true;
-        }else{
-            incorrectPassword.innerText = 'Incorrect Password';
-            event.preventDefault();
-        }
-    }
-})
